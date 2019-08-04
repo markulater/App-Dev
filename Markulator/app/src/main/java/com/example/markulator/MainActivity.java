962e770b172671638cc10e0486ef96f22ffe1403
MarkulaterApp <Markulater@outlook.com>
2019-08-04 16:33:12 +0100

    Update 0.4
    some of the ids have been corrected and the calculation has been added for the first section. Where the percentage is divided by 100.

--- Markulator/app/src/main/java/com/example/markulator/MainActivity.java
+++ Markulator/app/src/main/java/com/example/markulator/MainActivity.java
@@ -8,14 +8,26 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText Weight;
-    private EditText Result;
-    private TextView Outcome;
+    private EditText weight;
+    private EditText result;
+    private TextView outcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weight = (EditText) findViewById(R.id.weight);
+        result = (EditText) findViewById(R.id.result);
+        outcome = (TextView) findViewById(R.id.outcome);
+    }
 
+    public void markulutorCal(View v){
+        String weightStr = weight.getText().toString();
+        String resultStr = result.getText().toString();
+
+        if (weightStr != null && !"".equals(weightStr)
+            && weightStr != null && !"".equals(weightStr)){
+            int weightValue = int.ParseInt(weightStr/ 100;
+        }
     }
 }
